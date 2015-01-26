d6556136fa314d38f3fdf315792d01c82a72c070
GLEN <glenkeane.94@gmail.com>
2015-01-26 09:26:51 +0000

    fix issue of npm test failing on os's other than linux.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var LttngStream = require('./lib/lttng-stream.js');
-
 module.exports = {
   createBunyanStream: function createBunyanStream(opts) {
   	if("linux"!==process.platform) return process.stdout;
+  	
+  	var LttngStream = require('./lib/lttng-stream.js');
     return new LttngStream(opts);
   }
 };
